1212ea65c5a168198a62691f712ba08e627d4717
Francisco Andaur <francisco.andaur@icloud.com>
2020-06-29 19:31:56 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,3 @@
 class Post
-  attr_accessor :post, :title,:author
-  def initialize
-    @post = post
-    @title = "Hello World"
-    @author = author
-  end
-  
-  
-ends
+  attr_accessor :title, :author
+end

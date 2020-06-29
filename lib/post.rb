b5f1dbe7d7468ae2f8e3190e50637e76056229f0
Francisco Andaur <francisco.andaur@icloud.com>
2020-06-29 19:30:16 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,11 +2,8 @@ class Post
   attr_accessor :post, :title
   def initialize
     @post = post
-    @title = title
+    @title = "Hello World"
   end
   
-  def title
-    puts "Hello World"
-  end
   
 end
